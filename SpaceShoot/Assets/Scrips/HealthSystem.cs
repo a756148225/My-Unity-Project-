b18db2ab14fce73d0b98daa6ebf291267418477b
a756148225 <55566157+a756148225@users.noreply.github.com>
2020-07-17 20:24:11 +0800

    雷电

---
+++ SpaceShoot/Assets/Scrips/HealthSystem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour {
+
+    public int Health = 3;
+    public GameObject[] HealthBar;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="Enemy")
+        {
+            Health--;
+            Compare();
+        }
+    }
+    void Compare()
+    {
+        if(Health==2)
+        {
+            HealthBar[0].SetActive(false);
+        }
+
+        if(Health==1)
+        {
+            HealthBar[1].SetActive(false);
+        }
+        if(Health==0)
+        {
+            HealthBar[2].SetActive(false);
+        }
+        if(Health<0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
